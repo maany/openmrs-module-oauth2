208076a45f1860882482275723e39d2ee8fc0eff
Mayank Sharma <imptodefeat@gmail.com>
2015-07-11 10:29:17 +0530

    URL Mapping succeeded. URL for OAuth module being intercepted successfully

---
+++ api/src/main/java/org/openmrs/module/oauth2/config/OAuth2ServerConfig.java
@@ -0,0 +1,12 @@
+package org.openmrs.module.oauth2.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by OPSKMC on 7/9/15.
+ */
+@Configuration
+
+public class OAuth2ServerConfig {
+//    protected static class AuthorizationServerConfiguration
+}
