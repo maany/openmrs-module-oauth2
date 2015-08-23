c14348ebf7c51945f82766b06df94166549f19d7
Mayank Sharma <imptodefeat@gmail.com>
2015-08-23 14:33:14 +0530

    Token Persistence complete with spring-jdbc Resource Owner Password Credentials tested

--- api/src/main/java/org/openmrs/module/oauth2/api/model/Parametrized.java
+++ api/src/main/java/org/openmrs/module/oauth2/api/model/Parametrized.java
@@ -2,10 +2,12 @@ package org.openmrs.module.oauth2.api.model;
 
 import org.openmrs.module.oauth2.Client;
 
+import java.io.Serializable;
+
 /**
  * Created by OPSKMC on 8/19/15.
  */
-public interface Parametrized {
+public interface Parametrized extends Serializable {
     public String getParameter();
 
     public void setParameter(String parameter);
